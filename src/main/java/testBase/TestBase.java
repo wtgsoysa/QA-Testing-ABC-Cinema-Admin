898b20fb7ea6538e630516c34a342243d6bedf23
wtgsoysa <thanugasoysa2003@gmail.com>
2025-01-01 19:50:47 +0530

    feat: Add TestBase class with WebDriver initialization and teardown methods

--- src/main/java/testBase/TestBase.java
+++ src/main/java/testBase/TestBase.java
@@ -1,4 +1,23 @@
 package testBase;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class TestBase {
+
+    public static WebDriver driver;
+
+    public void initializerDriver(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("http://localhost:8080/ABC_Cinema_Admin_Web_exploded/");
+    }
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
