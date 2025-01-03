d1412780ee501be02d861d85c14696be0ffe227e
wtgsoysa <thanugasoysa2003@gmail.com>
2025-01-03 23:16:53 +0530

    feat(AddMovie) : naviagate to the add movie page

--- src/main/java/pages/AddMoviePage.java
+++ src/main/java/pages/AddMoviePage.java
@@ -1,8 +1,26 @@
 package pages;
 
-public class AddMoviePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class AddMoviePage {
+    WebDriver driver;
     //Locators
+
+    private By addMovieBtn = By.xpath("/html/body/div[1]/main/header/form/button");
+    private By addMovieTitle = By.xpath("/html/body/header/h1");
+
+    public AddMoviePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickAddMovieBtn() {
+        driver.findElement(addMovieBtn).click();
+    }
+
+    public String getAddMovieTitle() {
+        return driver.findElement(addMovieTitle).getText();
+    }
 
 
 }
--- src/test/java/tests/AddMovieTest.java
+++ src/test/java/tests/AddMovieTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.AddMoviePage;
 import pages.LoginPage;
 import pages.SideBar;
 import testBase.TestBase;
@@ -10,12 +11,14 @@ public class AddMovieTest extends TestBase {
 
     LoginPage loginPage;
     SideBar sideBarTest;
+    AddMoviePage addMoviePage;
 
     @BeforeMethod
     public void setUp() {
         initializerDriver();
         sideBarTest = new SideBar(driver);
         loginPage = new LoginPage(driver);
+        addMoviePage = new AddMoviePage(driver);
 
         loginPage.enterUsername("ABCAdmin"); // Correct credentials
         loginPage.enterPassword("ABCAdmin123");
@@ -24,7 +27,7 @@ public class AddMovieTest extends TestBase {
     }
 
     @Test(priority = 1)
-    public void clickOnMovieManagement() {
+    public void addMovie() {
 
         System.out.println("\n------------------------|| ABC_CINEMA_ADMIN_PANEL_ADD MOVIE_TEST || -----------------------\n");
 
@@ -44,6 +47,17 @@ public class AddMovieTest extends TestBase {
 
         System.out.println("----------------TC 001----------------\n");
         System.out.println("ADD MOVIE TEST CASES : \n");
+
+        addMoviePage.clickAddMovieBtn();
+
+        String expectedText2 = "Add Your Movie Details";
+        String actualText2 = addMoviePage.getAddMovieTitle();
+        if (expectedText2.equals(actualText2)) {
+            System.out.println("\t ------->  Navigate Add Movie Page Successfully\n\n");
+        }
+        else{
+            System.out.println("\t ------->  Navigate Add Movie Page Unsuccessfully\n\n");
+        }
 
 
     }
